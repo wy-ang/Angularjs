eb69c57055457f980ce56b5ae2e6b920ea4dcad8
wangyang <wyang@yy.com>
2015-07-08 14:30:22 +0800

    test

--- js/myAngular.js
+++ js/myAngular.js
@@ -3,14 +3,14 @@ app.controller("controller1",function($scope){
 	$scope.a = 1;
 	$scope.b = 2;
 })
-app.controller('controller2',function($scope){
+/*app.controller('controller2',function($scope){
 	var listArr=[{nameX:'hehe'},{nameX:'haha'},{nameX:'cao'}];
 	$scope.list = listArr;
 })
 
 app.controller('controller3',function($scope,$http){
 	$http.get("Customers_JSON.json").success(function(response){$scope.names = response});
-})
+})*/
 
 /*app.controller('myShow',function($scope){
 	$scope.myVie = true;
