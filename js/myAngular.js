92ab3c908b1abc146415c550a1a35af03a35302c
wangyang <wyang@yy.com>
2015-07-08 13:11:33 +0800

    new project

---
+++ js/myAngular.js
@@ -0,0 +1,20 @@
+var app = angular.module("myController",[]);
+app.controller("controller1",function($scope){
+	$scope.a = 1;
+	$scope.b = 2;
+})
+app.controller('controller2',function($scope){
+	var listArr=[{nameX:'hehe'},{nameX:'haha'},{nameX:'cao'}];
+	$scope.list = listArr;
+})
+
+app.controller('controller3',function($scope,$http){
+	$http.get("Customers_JSON.json").success(function(response){$scope.names = response});
+})
+
+app.controller('myShow',function($scope){
+	$scope.myVie = true;
+	$scope.toggle = function(){
+		$scope.myVie = !$scope.myVie;
+	}
+})
